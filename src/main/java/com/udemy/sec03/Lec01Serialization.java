0fc204e390080372580719091a843e0f83224445
termax-ironman <mrinalchandaml2@gmail.com>
2025-12-26 23:12:47 +0530

    serialization and deserialization

--- src/main/java/com/udemy/sec03/Lec01Serialization.java
+++ src/main/java/com/udemy/sec03/Lec01Serialization.java
@@ -5,10 +5,15 @@ import io.grpc.internal.LogExceptionRunnable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Lec01Serialization {
     private static final Logger log = LoggerFactory.getLogger(Lec01Scaler.class);
+    private static final Path PATH = Path.of("Person.out");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         var person1 = Person.newBuilder()
                 .setLastNamme("Chanda")
@@ -19,7 +24,17 @@ public class Lec01Serialization {
                 .setSalary(10)
                 .setBankAccountNumber(212121782329L)
                 .build();
+        serialize(person1);
+        log.info("{}", deserialize());
+        log.info("equals: {}", person1.equals(deserialize()));
+        log.info("bytearray size: {}", person1.toByteArray().length);
 
-        
+    }
+    public static void serialize(Person person) throws IOException {
+        person.writeTo(Files.newOutputStream(PATH));
+    }
+    public static Person deserialize() throws IOException {
+
+        return Person.parseFrom(Files.newInputStream(PATH));
     }
 }
